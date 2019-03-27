129469dae71303b88c401ae998985732bdfe70ba
walchko <walchko@users.noreply.github.com>
2019-03-27 18:29:35 -0400

    up

--- gecko/main.cpp
+++ gecko/main.cpp
@@ -1,13 +1,46 @@
+// Example with Gecko
+// make sure to run a pygeckocore.py instance with key: dalek.
+// MIT License Kevin J Walchko 2019
+//
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string>
+#include <vector>
 #include "gecko.hpp"
 #include "CYdLidar.h"
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 using namespace std;
 using namespace ydlidar;
 
+
 int main(){
+
+    json j = {
+      {"pi", 3.141},
+      {"happy", true},
+      {"name", "Niels"},
+      {"nothing", nullptr},
+      {"answer", {
+        {"everything", 42}
+      }},
+      {"list", {1, 0, 2}},
+      {"object", {
+        {"currency", "USD"},
+        {"value", 42.99}
+      }}
+    };
+
+    vector<double> pts = {1,2,3,4.5,6.789};
+    j["pts"] = pts;
+    j["bob"] = 3.456;
+
+    string sj = j.dump();
+    printf(">> %s\n", sj.c_str());
+
+
     printf("hello\n");
     gecko::init();
 
@@ -30,12 +63,15 @@ int main(){
     laser.setFixedResolution(false);
     laser.initialize();
 
-    Publisher *p = gecko::pubBindTCP("local", "bob2");
-
+    Publisher *p = gecko::pubBindTCP("dalek", "lidar");
+    if (p == nullptr) {
+        printf("*** invalid publisher ***\n");
+        return 1;
+    }
 
     unsigned long long start = 0;
 
-    while(ydlidar::ok()){
+    while(gecko::ok()){
         bool hardError;
         LaserScan scan;
 
